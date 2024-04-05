3f6d9a57d9dfbdbc08073354cef306a30523e29b
Chetainya <chetainyamanchanda@gmail.com>
2024-04-05 21:33:50 +0530

    db connection

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require('mongoose')
+
 
 const app = express();
 
+
+dotenv.config();
+
+mongoose.connect("mongodb://localhost:27017/realestate").then(() => console.log('DB connected')).catch((err) => console.log(err));
+
+
 app.get('/' , (req , res) => {
-    return res.send("Hello from server")
+    return res.send("Hello from server");
 })
 
 app.listen(3000 , () => console.log("Server running on PORT:3000"));
+
