8099ac1daf109c4132f108a69af19f3f08653f71
Chetainya <chetainyamanchanda@gmail.com>
2024-04-05 17:47:54 +0530

    created Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Profile from './Pages/Profile'
+import About from './Pages/About'
+import Header from './Components/Header'
 
 function App() {
   return (
-    <div>
-      <h1 className='text-red-500'>App</h1>
-    </div>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,35 @@
+import {FaSearch} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
+function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex flex-row justify-between  font-bold sm:text-xl p-3 max-w-6xl mx-auto items-center'>
+        <Link to='/'>
+        <h1 className='flex flex-wrap'>
+            <span className='text-slate-500'>MERN</span>
+            <span className='text-slate-700'>RealEstate</span>
+        </h1>
+        </Link>
+        <form className='flex items-center bg-slate-100 rounded-md p-3'>
+            <input type='text' placeholder='Search' className='bg-transparent focus:outline-none w-24 sm:w-64'></input>
+            <FaSearch className='text-slate-600'></FaSearch>
+        </form>
+        <ul className='flex gap-4'>
+            <Link to="/">
+        <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li>
+        </Link>
+        <Link to="/about">
+        <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li>
+        </Link>
+        <Link to="/signin">
+        <li className=' text-slate-700 hover:underline cursor-pointer'>SignIn</li>
+        </Link>
+      </ul>
+      </div>
+      
+    </header>
+  )
+}
+
+export default Header
